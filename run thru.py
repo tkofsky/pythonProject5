aca6914ea18158c3b38a69aaf3b2a42d9c3f88ad
tkofsky <kofsky.tev@gmail.com>
2023-05-15 16:45:07 -0400

    next2

--- run thru.py
+++ run thru.py
@@ -12,7 +12,7 @@ from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
 import os
 import calendar
-#####120
+#####1201
 htmldir = 'c:\weekly tsdrtxt'
 filename = 'refusalforeign.txt'
 thehtmlfile = os.path.join(htmldir, filename)

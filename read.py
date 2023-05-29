985b4f4d58d1c345dbcf61093396545a8c9150cd
tkofsky <kofsky.tev@gmail.com>
2023-05-29 16:42:59 -0400

    ocr

--- read.py
+++ read.py
@@ -1,18 +1,27 @@
 import requests
 import re
 import PyPDF2
+import fitz,os
+import pytesseract
+from pdf2image import convert_from_path
 
-
-def pdf_to_text(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
-        text = ''
-        for page in range(reader.numPages):
-            text += reader.getPage(page).extractText()
-
+def pdf_to_text(filepath):
+    text = ""
+    try:
+        with fitz.open(filepath) as doc:
+            text = ""
+            for page in doc:
+                text += page.get_text().strip()
+    except:
+        print("An exception occurred")
     return text
 
-
+def pdf_ocr(pdf_path):
+    images = convert_from_path(pdf_path)
+    text = ""
+    for image in images:
+        text += pytesseract.image_to_string(image)
+    return text
 
 file = open('refusals.txt', 'r',encoding='utf-8')
 txt = file.read()
@@ -33,7 +42,8 @@ for match in matches:
 
     with open("thepdf.pdf", 'wb') as f:
         f.write(pdfdata)
-    text = pdf_to_text("thepdf.pdf")
+        pdf_ocr("thepdf.pdf")
+    #text = pdf_to_text("thepdf.pdf")
     print (text)
 
 

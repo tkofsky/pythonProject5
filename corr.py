a2b1efa8b7a368c2ccc1a0c694120e44d63d3c0f
tkofsky <kofsky.tev@gmail.com>
2023-06-02 10:08:01 -0400

    corr coref non lin regression

--- corr.py
+++ corr.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
 # Define your two lists of numbers
@@ -9,3 +10,17 @@ correlation_coefficient = np.corrcoef(list1, list2)[0, 1]
 
 # Print the correlation coefficient
 print("Correlation coefficient:", correlation_coefficient)
+
+X = np.array(list1).reshape(-1, 1)
+y = np.array(list2)
+
+# Fit a linear regression model
+regression_model = LinearRegression()
+regression_model.fit(X, y)
+
+# Predict new data points based on list1
+new_data = [6, 7, 8]
+predicted_values = regression_model.predict(np.array(new_data).reshape(-1, 1))
+
+# Print the predicted values
+print("Predicted values:", predicted_values)

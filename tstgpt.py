2b395513cf0029ea6f211a4ff34ee5a258fd9e6b
tkofsky <kofsky.tev@gmail.com>
2023-07-17 11:38:48 -0400

    new chat gpt3

--- tstgpt.py
+++ tstgpt.py
@@ -10,7 +10,7 @@ from langchain.chat_models import ChatOpenAI
 os.environ["OPENAI_API_KEY"]=""
 #
 #query = sys.argv[1]
-query ="what day is it"
+query ="what day is it today"
 print (query)
 loader = TextLoader('data.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])

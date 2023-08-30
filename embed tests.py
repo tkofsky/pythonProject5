93501290eb96d2140588ef8b6212b498ae7f63f7
tkofsky <kofsky.tev@gmail.com>
2023-08-30 14:41:13 -0400

    next load

---
+++ embed tests.py
@@ -0,0 +1,33 @@
+import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import TensorflowHubEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
+
+###################################Sentence Transformers Embeddings - done one CPU
+text = "This is a test query."
+embeddings = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
+query_result = embeddings.embed_query(text)
+print(query_result)
+print(len(query_result))
+
+#################################################Tensor Flow
+text = "This is a test query."
+embeddings_tf = TensorflowHubEmbeddings()
+query_result= embeddings_tf.embed_query(text)
+print(query_result)
+print(len(query_result))
+
+###################################################OPENAI
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-ada-002",
+    openai_api_key=""
+)
+text = "This is a test query.OpenAI uses 1,536 dimensions for embeddings in the text-embedding-ada-002 model due to multiple design choices driven by the trade-offs between model complexity, computational efficiency, and predictive performance. 1. Model complexity: Higher-dimensional embeddings can capture more complex and nuanced relationships in the data. This allows the model to better understand and represent various linguistic patterns, syntactic properties, and semantic relationships. 2. Computational efficiency: Though higher-dimensional embeddings provide richer"
+query_result = embeddings.embed_query(text)
+print(query_result)
+print(len(query_result))
+
+documents = ["This is a sample document.", "This is another sample document."]
+document_embeddings = embeddings.embed_documents(texts=documents, chunk_size=1000)
+print(document_embeddings)
+

d3e17c2f6137b2d36b20e7f60b6f876d5b6458e1
tkofsky <kofsky.tev@gmail.com>
2023-09-07 11:22:32 -0400

    new options

--- selnumwithhide.py
+++ selnumwithhide.py
@@ -1,7 +1,9 @@
 if __name__ == '__main__':
 
     import undetected_chromedriver as uc
-    driver = uc.Chrome(headless=False,use_subprocess=False)
+    uc.options.binary_location =r"chromedriver.exe"
+    driver = uc.Chrome(headless=False,use_subprocess=False,driver_executable_path="chromedriver.exe")
+
     #driver.get('https://www5.services.mrq.gouv.qc.ca/MrqAnonyme/BR/BR01/BR01A1_01A_ConsulterBNR_PC/P_Consultation.aspx?CLNG=A#Recherche1_K1Ancre1')
-    driver.get("https://bot.sannysoft.com/")
+    driver.get("https://opensea.io/rankings/trending/")
     driver.save_screenshot('nowsecure.png')

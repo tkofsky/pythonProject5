ea2b59e6bf53e745af031b766bca76c4f8402a05
tkofsky <kofsky.tev@gmail.com>
2024-05-09 11:13:43 -0400

    query open ai

--- tstgpt.py
+++ tstgpt.py
@@ -1,10 +1,10 @@
+
+from config import OPENAI_API_KEY
 from langchain.llms import OpenAI
 import os
 import sys
 
-
 query = input("Enter doc query? ")
-
 
 
 from langchain.document_loaders import TextLoader
@@ -12,32 +12,36 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+
 from langchain.document_loaders import WebBaseLoader
-
+#os.environ["OPENAI_API_KEY"]="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"
+os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
 import time
-#change 2
+
+# change 2
+
+
+# query = sys.argv[1]
+print(OPENAI_API_KEY)
+
+query = "what team has the most Points Per Game"  #
+query = "how many wins do the Boston Celtics Have"  #
+query = "how many losses to the Golden State Worriors have"  #
+query = "who had more wins the Golden State Worriors or Los Angles Lakers"  #
+query = "which teams have less than than 35 wins"  #
+query = "which team has the most wins in the Pacific Division"  #
+
+query = "how many loses (L) does Dallas"  #
 #
-#query = sys.argv[1]
-query ="what team has the most Points Per Game" #
-query ="how many wins do the Boston Celtics Have" #
-query ="how many losses to the Golden State Worriors have" #
-query ="who had more wins the Golden State Worriors or Los Angles Lakers" #
-query ="which teams have less than than 35 wins" #
-query ="which team has the most wins in the Pacific Division" #
 
+print(query)
+filename = os.path.join("csv files", "2.csv")  # csv file taken from csv table.py
 
- #
-
-print (query)
-filename = os.path.join("csv files", "2.csv") # csv file taken from csv table.py
 loader = TextLoader(filename)
-#loader = WebBaseLoader("https://www.tsn.ca/nhl/standings")
-#loader = TextLoader('3.html')
+# loader = WebBaseLoader("https://www.tsn.ca/nhl/standings")
+# loader = TextLoader('3.html')
 
 
 index = VectorstoreIndexCreator().from_loaders([loader])
 
-print(index.query(query,llm=ChatOpenAI()))
-
-
-
+print(index.query(query, llm=ChatOpenAI()))

7b11e3a3324a13c706f738c676fdd9fea0734414
tkofsky <kofsky.tev@gmail.com>
2024-08-15 15:06:26 -0400

    rerank with rag

--- rag improve.py
+++ rag improve.py
@@ -1,3 +1,5 @@
+import IPython
+from IPython.display import Markdown, display
 import os
 import openai
 from openai import OpenAI
@@ -6,7 +8,16 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from llama_index.core.prompts import PromptTemplate
+from llama_index.core.response.notebook_utils import display_source_node
 openai.api_key = os.getenv("OPENAI_API_KEY")
+from llama_index.core import (VectorStoreIndex,ServiceContext, download_loader)
+
+import llama_index.core
+
+
+import pip
+#pip.main(['install',"git+https://github.com/FlagOpen/FlagEmbedding.git"])
+
 
 
 # Import packages
@@ -54,3 +65,60 @@ response = gpt3.complete(prompt)
 print(str(response))
 
 
+# modify default values of chunk size and chunk overlap
+service_context_gpt3 = ServiceContext.from_defaults(llm=gpt3, chunk_size = 512, chunk_overlap=50, embed_model=embed_model)
+
+# build index
+index = VectorStoreIndex.from_documents(
+    documents, service_context=service_context_gpt3
+
+)
+# returns the engine for the index
+query_engine = index.as_query_engine(similarity_top_k=4)
+
+####response = query_engine.query("What is the plot of the film that led Emma Stone to win her first Academy Award?")
+response = query_engine.query("Compare the families of Emma Stone and Ryan Gosling")
+print(response)
+
+
+
+
+# Retrieve the top three chunks for the second query
+retriever = index.as_retriever(similarity_top_k=3)
+query = "Compare the families of Emma Stone and Ryan Gosling"
+nodes = retriever.retrieve(query)
+# Print the chunks
+for node in nodes:
+    print('----------------------------------------------------')
+    print (node)#####display_source_node(node, source_length = 500)
+
+
+
+#########################################
+
+from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+
+from llama_index.core.schema import QueryBundle
+# Re-Rank chunks based on the bge-reranker-base-model
+reranker = FlagEmbeddingReranker(
+    top_n = 3,
+    model = "BAAI/bge-reranker-base",
+)
+# Return the updated chunks
+query_bundle = QueryBundle(query_str=query)
+ranked_nodes = reranker._postprocess_nodes(nodes, query_bundle = query_bundle)
+for ranked_node in ranked_nodes:
+    print('----------------------------------------------------')
+    print(ranked_node)
+################################
+
+# Initialize the query engine with Re-Ranking
+query_engine = index.as_query_engine(
+    similarity_top_k = 3,
+    node_postprocessors=[reranker]
+)
+
+# Print the response from the model
+response = query_engine.query("Compare the families of Emma Stone and Ryan Gosling")
+
+print(response)

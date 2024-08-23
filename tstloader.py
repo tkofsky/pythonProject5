92f14c9843bda5b86c8398f6e7031eda3b1dda42
tkofsky <kofsky.tev@gmail.com>
2024-08-23 16:10:58 -0400

    load multi url

---
+++ tstloader.py
@@ -0,0 +1,22 @@
+import os
+from langchain_openai import ChatOpenAI #model
+from langchain.globals import set_llm_cache #response caching
+from langchain.cache import InMemoryCache
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate #prompt templating
+from langchain.document_loaders import WikipediaLoader #document loader
+from langchain_community.document_loaders import WebBaseLoader
+import nest_asyncio
+import os
+nest_asyncio.apply()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+loader = WebBaseLoader(["https://www.espn.com/", "https://google.com"])
+loader.requests_per_second = 1
+docs = loader.aload()
+
+
+print (docs)
+
+

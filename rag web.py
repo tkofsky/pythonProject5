fabd7209477e4f55c2178da1b104bd723e635487
tkofsky <kofsky.tev@gmail.com>
2024-08-28 14:25:38 -0400

    scrapfy reader

---
+++ rag web.py
@@ -0,0 +1,29 @@
+import os
+import llama_index.readers
+
+from llama_index.readers.web import ScrapflyReader
+from llama_index.core import VectorStoreIndex
+
+scrapfly_reader = ScrapflyReader(
+    api_key="Your Scrapfly API key",
+
+)
+
+# Load documents from URLs as markdown
+documents = scrapfly_reader.load_data(
+    urls=["https://web-scraping.dev/products"]
+)
+
+# Set the OpenAI key as a environment variable
+OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+
+# Create an index store for the documents
+index = VectorStoreIndex.from_documents(documents)
+
+# Create the RAG engine with using the index store
+query_engine = index.as_query_engine()
+
+# Submit a query
+response = query_engine.query("What is the flavor of the dark energy potion?")
+print(response)
+"The flavor of the dark energy potion is bold cherry cola."
